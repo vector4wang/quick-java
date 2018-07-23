9df750eb2813b2915cdaa2494cbd11330ab5f2b7
vector <vector.wang@hirede.com>
2018-07-23 16:27:03 +0800

    '看清题目，理清思路'

--- src/leetcode/array/Intersect.java
+++ src/leetcode/array/Intersect.java
@@ -1,5 +1,9 @@
 package leetcode.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created with IDEA
  * User: vector 
@@ -7,14 +11,15 @@ package leetcode.array;
  * Time: 17:02
  * Description:
  *
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/26/
  * 给定两个数组，写一个方法来计算它们的交集。
  *
  * 给定 nums1 = [1, 2, 2, 1], nums2 = [2, 2], 返回 [2, 2].
  */
 public class Intersect {
 	public static void main(String[] args) {
-		int[] nums1 = {1, 2, 2, 1};
-		int[] nums2 = {2, 2};
+		int[] nums1 = {2,1,3,1,3};
+		int[] nums2 = {2,2,3};
 
 		int[] intersect = intersect(nums1, nums2);
 		for (int i : intersect) {
@@ -22,16 +27,30 @@ public class Intersect {
 		}
 	}
 
-	// TODO: 2018/7/18 0018  
 	public static int[] intersect(int[] nums1, int[] nums2) {
-		int[] result = {};
+		Arrays.sort(nums1);
+		Arrays.sort(nums2);
 
+		int x = 0;
+		int y = 0;
+		List<Integer> result = new ArrayList<>();
+		while (x < nums1.length && y < nums2.length) {
 
-		for (int i = 0; i < nums1.length; i++) {
-			for (int j = 0; j < nums2.length; j++) {
-				
+			if (nums1[x] > nums2[y]) {
+				y++;
+			} else if (nums1[x] < nums2[y]) {
+				x++;
+			} else {
+				result.add(nums1[x]);
+				x++;
+				y++;
 			}
 		}
-		return result;
+
+		int[] tmp = new int[result.size()];
+		for (int k = 0; k < tmp.length; k++) {
+			tmp[k] = result.get(k);
+		}
+		return tmp;
 	}
 }
---
+++ src/leetcode/array/PlusOne.java
@@ -0,0 +1,39 @@
+package leetcode.array;
+
+/**
+ * Created with IDEA
+ * User: vector
+ * Data: 2018/7/23 0023
+ * Time: 15:54
+ * Description:
+ *
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/27/
+ * 给定一个非负整数组成的非空数组，在该数的基础上加一，返回一个新的数组。
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ *
+ */
+public class PlusOne {
+	public static void main(String[] args) {
+		int[] ints = plusOne(new int[]{9,9,9,9});
+
+
+		for (int anInt : ints) {
+			System.out.println(anInt);
+		}
+	}
+
+	public static int[] plusOne(int[] digits) {
+		int n = digits.length;
+		for (int i = digits.length - 1; i >= 0; --i) {
+			if (digits[i] < 9) {
+				++digits[i];
+				return digits;
+			}
+			digits[i] = 0;
+		}
+		int[] res = new int[n + 1];
+		res[0] = 1;
+		return res;
+	}
+}
