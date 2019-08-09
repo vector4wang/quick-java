b8c009ed95ddb32f69bda5b5aa3745e3cd3ac8aa
vector <vector4wang@qq.com>
2019-08-09 08:24:23 +0800

    feat: 无重复字符的最长子串

---
+++ leetcode-learning/src/main/java/com/leetcode/learn/string/LengthOfLongestSubstring.java
@@ -0,0 +1,23 @@
+package com.leetcode.learn.string;
+
+/**
+ *给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * @author wangxc
+ * @date: 2019-08-09 07:58
+ *
+ */
+public class LengthOfLongestSubstring {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static int lengthOfLongestSubstring(String s) {
+		int max=0;
+		for (int i = 0; i < s.length()-1; i++) {
+			for (int j = i+1; j < s.length(); j++) {
+				
+			}
+		}
+	}
+}
