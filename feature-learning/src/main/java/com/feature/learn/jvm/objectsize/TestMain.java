1e0e5d755520ab091369fdc10e8283b221ceb111
Vector Wang <vector.wang@hirede.com>
2019-09-18 20:39:22 +0800

    feat: java 高位转低位溢出的计算过程

--- feature-learning/src/main/java/com/feature/learn/jvm/objectsize/TestMain.java
+++ feature-learning/src/main/java/com/feature/learn/jvm/objectsize/TestMain.java
@@ -6,63 +6,35 @@ package com.feature.learn.jvm.objectsize;
  */
 public class TestMain {
     public static void main(String[] args) {
-		/**
-		 * java中强制类型转换时，高位数截取成低位数的方法
-		 * 233: 00000000 00000000 00000000 11101001
-		 * int 4字节 32 位
-		 * byte 1字节 8位
-		 * 去低八位 11101001
-		 * 最高位位1 即为负数
-		 */
-    	byte b1 = (byte) 233;
+        /**
+         * java中强制类型转换时，高位数截取成低位数的方法
+         * 233: 00000000 00000000 00000000 11101001
+         * int 4字节 32 位
+         * byte 1字节 8位
+         * 去低八位 11101001
+         * 最高位位1 即为负数
+         */
+        byte b1 = (byte) 233;
 
-        System.out.println((byte)233); // -23
-        System.out.println((byte)-233); // 23
-		System.out.println("~b2: " + ~10); // -11
+        System.out.println((byte) 129);
+        System.out.println((byte) -129);
+        System.out.println("~b2: " + ~10);
 
 
         float af = 1f;
         float bf = 0.9f;
 
-        float ff = af-bf;
+        float ff = af - bf;
 
         System.out.println(ff);
 
-
-        short  s1 = 1;
-//        s1 = s1 + 1;
+        byte s1 = 127;
+        s1 = (byte) (s1 + 1);
+        System.out.println("s1: " + s1);
 
         s1 += 1;
 
         System.out.println(s1);
-
-//
-//        byte b = 1;
-//        Byte bb = 1;
-//
-//        char c = 1;
-//        Character bc = 1;
-//
-//        short s = 1;
-//        Short bs = 1;
-//
-//        int i = 1;
-//        Integer bi = 1;
-//
-//
-//        double d = 1;
-//        Double bd = 1D;
-//
-//        float f = 1;
-//        Float bf = 1F;
-//
-//        long l = 1;
-//        Long bl = 1L;
-//
-//        boolean bn = false;
-//        Boolean bbn = false;
-
-
 
 
     }
