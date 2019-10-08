94d6fa7daef24ed5aa633200fabc0a28fb671a6f
vector <vector4wang@qq.com>
2019-10-09 07:28:39 +0800

    lll

---
+++ feature-learning/src/main/java/com/feature/learn/classload/ClassEquals.java
@@ -0,0 +1,15 @@
+package com.feature.learn.classload;
+
+/**
+ *
+ * @author wangxc
+ * @date: 2019-09-29 08:06
+ *
+ */
+public class ClassEquals {
+	public static void main(String[] args) {
+		String a = "A";
+		String b = "B";
+		System.out.println(a.equals(b));
+	}
+}
---
+++ feature-learning/src/main/java/com/feature/learn/classload/SuperClass.java
@@ -0,0 +1,29 @@
+package com.feature.learn.classload;
+
+/**
+ *被动使用类字段演示一：
+ *通过子类引用父类的静态字段，不会导致子类初始化
+ **/
+public class SuperClass {
+	static {
+		System.out.println("SuperClass init！");
+	}
+
+
+}
+
+class SubClass extends SuperClass {
+	public static int value = 123;
+	static {
+		System.out.println("SubClass init！");
+	}
+}
+
+/**
+ *非主动使用类字段演示
+ **/
+class NotInitialization {
+	public static void main(String[] args) {
+		System.out.println(SubClass.value);
+	}
+}
